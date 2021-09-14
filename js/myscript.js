b413d2c7ed30102eea5e993a5bfbabb428003c25
GerryFox22 <gerry.cuomo1994@gmail.com>
2021-09-14 15:34:39 +0200

    ultime modifiche

--- js/myscript.js
+++ js/myscript.js
@@ -6,13 +6,16 @@ var app = new Vue({
       message: 'Hello Vue!',
       titleClass : "text-center text-danger p-5",
       jumboSrc : "https://i.ytimg.com/vi/DsuTwV0jwaY/maxresdefault.jpg",
+      vueButton : "Clicca Qui !!",
     },
 
     methods: {
 
         changeJumboImg: function () {
             this.jumboSrc = "https://www.thespian.hr/png/delivering-vuejs-components-through-json-payload.png"
-            
+            this.vueButton = "GRAZIE VUE ♥"
           }
     },
   });
+
+  
