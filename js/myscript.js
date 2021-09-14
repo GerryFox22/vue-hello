5a9e3610f0cd1feeff93c43b3d77d7920159daf9
GerryFox22 <gerry.cuomo1994@gmail.com>
2021-09-14 15:08:28 +0200

    Inizializzazione e fine esercizio

---
+++ js/myscript.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+
+    el: '#app',
+
+    data: {
+      message: 'Hello Vue!',
+      titleClass : "text-center text-danger p-5",
+      jumboSrc : "https://i.ytimg.com/vi/DsuTwV0jwaY/maxresdefault.jpg",
+    },
+
+    methods: {
+
+        changeJumboImg: function () {
+            this.jumboSrc = "https://www.thespian.hr/png/delivering-vuejs-components-through-json-payload.png"
+            
+          }
+    },
+  });
